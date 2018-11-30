cee62ea43ddb5fd53f79e520e092ced506c175b6
wizubizu <odugbesanabimbola@gmail.com>
2018-11-30 17:07:30 +0100

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,43 @@
+// partner slider
+    $(document).ready(function(){
+        $('.customer-logos').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            arrows: false,
+            dots: false,
+            pauseOnHover: false,
+            responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4
+                }
+            }, {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 3
+                }
+            }]
+        });
+    });
+    // partner slider end
+
 // typed writer
 
-        document.addEventListener('DOMContentLoaded', function(){
+    document.addEventListener('DOMContentLoaded', function(){
 
-            Typed.new("#typed", {
-                stringsElement: document.getElementById('typed-strings'),
-                typeSpeed: 20,
-                backDelay: 600,
-                backSpeed: 60,
-                loop: true,
-                contentType: 'html', // or text
-                // defaults to null for infinite loop
-                loopCount: null,
-               
-            });
-
+        Typed.new("#typed", {
+            stringsElement: document.getElementById('typed-strings'),
+            typeSpeed: 20,
+            backDelay: 600,
+            backSpeed: 60,
+            loop: true,
+            contentType: 'html', // or text
+            // defaults to null for infinite loop
+            loopCount: null,
+           
         });
+
+    });
+
